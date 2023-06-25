c02d2eba88d6542b37c60e6c8bccfa1bde180af1
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-06-25 13:02:15 -0300

    Created the login and signup initial screens

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_evento_app/view/signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,8 +9,79 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _email = new TextEditingController();
+  final TextEditingController _senha = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "NOSSO EVENTO",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Container(
+            child: Form(
+              child: Column(
+                children: [
+                  Flexible(
+                      flex: 1,
+                      child: Image.asset(
+                        "assets/images/logo.png",
+                        width: 150,
+                        height: 150,
+                      )),
+                  Flexible(
+                      child: TextFormField(
+                    maxLength: 100,
+                    decoration: InputDecoration(hintText: "E-mail"),
+                    controller: _email,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (val) => val == "" ? val : null,
+                  )),
+                  Flexible(
+                      child: TextFormField(
+                    maxLength: 20,
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: "Senha"),
+                    controller: _senha,
+                    keyboardType: TextInputType.text,
+                    validator: (val) => val == "" ? val : null,
+                  )),
+                  Flexible(
+                      child: TextButton(
+                    onPressed: () {
+                      _login(context);
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateColor.resolveWith(
+                            (states) => Colors.deepPurple)),
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )),
+                  Flexible(
+                      child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) => SignupScreen()));
+                    },
+                    child: Text("Cadastrar"),
+                  )),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
+  void _login(BuildContext context) {}
 }
--- lib/view/signup_screen.dart
+++ lib/view/signup_screen.dart
@@ -8,8 +8,74 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _nome = TextEditingController();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _senha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "NOSSO EVENTO",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Form(
+            child: Column(
+              children: [
+                Flexible(
+                    flex: 1,
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      width: 150,
+                      height: 150,
+                    )),
+                Flexible(
+                    child: TextFormField(
+                  maxLength: 100,
+                  decoration: InputDecoration(hintText: "Nome"),
+                  controller: _nome,
+                  keyboardType: TextInputType.name,
+                  validator: (val) => val == "" ? val : null,
+                )),
+                Flexible(
+                    child: TextFormField(
+                  maxLength: 100,
+                  decoration: InputDecoration(hintText: "E-mail"),
+                  controller: _email,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (val) => val == "" ? val : null,
+                )),
+                Flexible(
+                    child: TextFormField(
+                  maxLength: 20,
+                  obscureText: true,
+                  decoration: InputDecoration(hintText: "Senha"),
+                  controller: _senha,
+                  keyboardType: TextInputType.text,
+                  validator: (val) => val == "" ? val : null,
+                )),
+                Flexible(
+                    child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.deepPurple)),
+                  onPressed: () {},
+                  child: Text(
+                    "Cadastrar",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
